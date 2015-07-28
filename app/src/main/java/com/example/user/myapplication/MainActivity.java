35a014214d420885b2b940148b62687f8e03bff7
kimyongbeom <manniere@naver.com>
2015-07-28 14:30:54 +0900

    test 변수

--- app/src/main/java/com/example/user/myapplication/MainActivity.java
+++ app/src/main/java/com/example/user/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.MenuItem;
 
 
 public class MainActivity extends Activity {
-
+    int abcdefg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
